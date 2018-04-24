36e092960f74492d676772a6d4f00752097615c5
Ben Cochez <ben@cochez.consulting>
2018-04-24 08:02:05 +0200

    fix test

--- spec/lib/formatter_spec.rb
+++ spec/lib/formatter_spec.rb
@@ -13,7 +13,6 @@ describe SimpleCov::Formatter::TeamcitySummaryFormatter do
   describe "#format_teamcity" do
     let(:simplecov_result) { OpenStruct.new({covered_percent: '12.34567', covered_lines: '1234', total_lines: '5678' })}
     let(:open_block) { "##teamcity[blockOpened name='Code Coverage Summary']" }
-    let(:coverage_percent) { "##teamcity[buildStatisticValue key='CodeCoverageL' value='12.34567']" }
     let(:covered_lines) { "##teamcity[buildStatisticValue key='CodeCoverageAbsLCovered' value='1234']" }
     let(:total_lines) { "##teamcity[buildStatisticValue key='CodeCoverageAbsLTotal' value='5678']" }
     let(:close_block) { "##teamcity[blockClosed name='Code Coverage Summary']" }
@@ -21,7 +20,6 @@ describe SimpleCov::Formatter::TeamcitySummaryFormatter do
     subject { described_class.new.format_teamcity(simplecov_result) }
 
     it { expect(subject).to include(open_block) }
-    it { expect(subject).to include(coverage_percent) }
     it { expect(subject).to include(covered_lines) }
     it { expect(subject).to include(total_lines) }
     it { expect(subject).to include(close_block) }
